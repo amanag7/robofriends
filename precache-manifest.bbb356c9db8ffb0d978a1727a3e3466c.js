74a15a4241e8fad559c11534f1a8b236886e0c9f
Aman Agrawal <sprshag@gmail.com>
2020-10-18 17:20:20 +0530

    Updates

---
+++ precache-manifest.bbb356c9db8ffb0d978a1727a3e3466c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2770e41e37abb6d764e02d0e1f1c6897",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6838f6713cee17b4ef6e",
+    "url": "/robofriends/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "36738bbceb6ac5b0ebf6",
+    "url": "/robofriends/static/js/2.3788da92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends/static/js/2.3788da92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6838f6713cee17b4ef6e",
+    "url": "/robofriends/static/js/main.a16b4b3a.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
